30aac14567b5f3313c5ba0237ef9adc8d762b0ec
ke-stack <kingkevo254@gmail.com>
2023-08-23 14:12:43 +0300

    This file contains a fetch method to a flask backed to fetch a json file. Contents of the json file are also displayed on the console

---
+++ json.js
@@ -0,0 +1,11 @@
+fetch('http://127.0.0.1:5002/students')
+  .then(response => response.json())
+
+  .then(data => {
+    console.log(data),
+    console.log(data[1]),
+    console.log(data[1].id);
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
